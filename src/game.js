0615f0b170e2ca5c0eb784d60350dbd8e499f8db
kadeem254 <kadeemalexander254@gmail.com>
2023-10-16 21:42:35 +0300

    fix: Use phaser from cdn
    - Phaser is included in the index.html from the cdn rather than being bundled in.

--- src/game.js
+++ src/game.js
@@ -1,14 +1,18 @@
 import './style.css';
-import Phaser from 'phaser';
 
-new Phaser.Game({
-  type: Phaser.AUTO,
-  width: 1920,
-  height: 1080,
+function onPageLoad(){
+  const Game = new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+  
+    parent: "game-container",
+    scale: {
+      mode: Phaser.Scale.ScaleModes.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+  })
+}
 
-  parent: "game-container",
-  scale: {
-    mode: Phaser.Scale.ScaleModes.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-  }
-})
+window.addEventListener('load', onPageLoad)
+
